3678d11eafb0b8aa84c848812c128b0e7ec24b0c
Anthony Höhn <75019251+anthohn@users.noreply.github.com>
2023-11-26 11:19:32 +0100

    dark mode switch

--- src/index.jsx
+++ src/index.jsx
@@ -4,35 +4,33 @@ import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Freelances from './pages/Freelances'
 import Error from './components/Error'
-import { createGlobalStyle } from 'styled-components'
-
-
-const GlobalStyle = createGlobalStyle`
-    * {
-      font-family: 'Trebuchet MS', Helvetica, sans-serif;
-    }
-
-    body {
-      margin: 0;
-    }
-`
+import GlobalStyle from './utils/style/GlobalStyle'
+import { ThemeProvider, SurveyProvider } from './utils/context'
+import Results from './pages/Results'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <GlobalStyle />
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Freelances" element={<Freelances />} />
-        <Route path="/survey/:questionNumber" element={<Survey />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <ThemeProvider>
+        <SurveyProvider>
+          <GlobalStyle />
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/survey/:questionNumber" element={<Survey />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="/freelances" element={<Freelances />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+          <Footer />
+        </SurveyProvider>
+      </ThemeProvider>
     </Router>
   </React.StrictMode>
 );
